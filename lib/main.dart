f6155e12f5be3600f667ba42ee0f4a37adcb5624
Faustmann, Gunnar (SGRE SE R NE&ME TE SCA) <gunnar.faustmann@siemensgamesa.com>
2022-10-21 16:35:11 +0200

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -41,33 +40,6 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
-    );
-  }
-}
-
-class MainPage extends ConsumerStatefulWidget {
-  static String get routeName => 'main';
-  static String get routeLocation => '/';
-  const MainPage({super.key});
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _HomePageState();
-}
-
-class _HomePageState extends ConsumerState<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(ref.watch(mainPageTitleProvider)),
-        actions: [
-          IconButton(
-            onPressed: () => context.go('/create_routine'),
-            icon: const Icon(Icons.add),
-          )
-        ],
       ),
     );
   }
--- lib/ui/pages/create_routine_page.dart
+++ lib/ui/pages/create_routine_page.dart
@@ -17,9 +17,17 @@ class CreateRoutinePage extends ConsumerStatefulWidget {
 }
 
 class _CreateRoutinPageState extends ConsumerState<CreateRoutinePage> {
+  List<String> categories = [
+    'work',
+    'vacation',
+    'sickleave'
+  ]; //TODO: replace with isar data
+  String dropdownValue = 'work'; //TODO: replace with isar data
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color(0xffffffff),
         appBar: AppBar(
           leading: IconButton(
             onPressed: () => context.go('/'),
@@ -29,11 +37,46 @@ class _CreateRoutinPageState extends ConsumerState<CreateRoutinePage> {
           title: Text(ref.watch(createRoutinePageTitleProvider)),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Text('Category'),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Category'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      child: DropdownButton(
+                        focusColor: const Color(0xffffffff),
+                        dropdownColor: const Color(0xffffffff),
+                        isExpanded: true,
+                        value: dropdownValue,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: categories
+                            .map<DropdownMenuItem<String>>((String item) {
+                          return DropdownMenuItem<String>(
+                            value: item,
+                            child: Text(item),
+                          );
+                        }).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            dropdownValue = value!;
+                          });
+                        },
+                      ),
+                    ),
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.add))
+                  ],
+                )
+              ],
+            ),
           ),
         ));
   }
 }
+
+//https://www.youtube.com/watch?v=44ziT-XdPbI&list=PLKKf8l1ne4_hMBtRykh9GCC4MMyteUTyf&index=9 
+// 30:52
