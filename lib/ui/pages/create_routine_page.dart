dd519f117dbc67c78ae1996f9c3d941e540ef2d1
Faustmann, Gunnar (SGRE SE R NE&ME TE SCA) <gunnar.faustmann@siemensgamesa.com>
2022-10-24 15:18:06 +0200

    isar does not load correctly

--- lib/ui/pages/create_routine_page.dart
+++ lib/ui/pages/create_routine_page.dart
@@ -17,12 +17,9 @@ class CreateRoutinePage extends ConsumerStatefulWidget {
 }
 
 class _CreateRoutinPageState extends ConsumerState<CreateRoutinePage> {
-  List<String> categories = [
-    'work',
-    'vacation',
-    'sickleave'
-  ]; //TODO: replace with isar data
-  String dropdownValue = 'work'; //TODO: replace with isar data
+  List<Category>? categories;
+  Category? dropdownValue;
+
   List<String> days = [
     'Monday',
     'Tuesday',
@@ -33,7 +30,14 @@ class _CreateRoutinPageState extends ConsumerState<CreateRoutinePage> {
     'Sunday',
   ]; //TODO: replace with isar data
   String dropdownValueDay = 'Monday'; //TODO: replace with isar data
+
   TimeOfDay selectedTime = TimeOfDay.now();
+
+  @override
+  void initState() {
+    super.initState();
+    _readCategory();
+  }
 
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _timeController = TextEditingController();
@@ -42,165 +46,167 @@ class _CreateRoutinPageState extends ConsumerState<CreateRoutinePage> {
   @override
   Widget build(BuildContext context) {
     final isar = ref.read(initDirIsarProvider);
-
     final width = MediaQuery.of(context).size.width * 0.7;
+
     return Scaffold(
-        backgroundColor: const Color(0xffffffff),
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () => context.go('/'),
-            icon: const Icon(Icons.arrow_back),
-          ),
-          centerTitle: true,
-          title: Text(ref.watch(createRoutinePageTitleProvider)),
+      backgroundColor: const Color(0xffffffff),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => context.go('/'),
+          icon: const Icon(Icons.arrow_back),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Category',
-                  style: TextStyle(
-                      color: Colors.black38, fontWeight: FontWeight.w600),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: width,
-                      child: DropdownButton(
-                        focusColor: const Color(0xffffffff),
-                        dropdownColor: const Color(0xffffffff),
-                        isExpanded: true,
-                        value: dropdownValue,
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        items: categories
-                            .map<DropdownMenuItem<String>>((String item) {
-                          return DropdownMenuItem<String>(
-                            value: item,
-                            child: Text(item),
-                          );
-                        }).toList(),
-                        onChanged: (String? value) {
-                          setState(() {
-                            dropdownValue = value!;
-                          });
-                        },
+        centerTitle: true,
+        title: Text(ref.watch(createRoutinePageTitleProvider)),
+      ),
+      body: isar.when(
+        data: (isar) {
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Category',
+                    style: TextStyle(
+                        color: Colors.black38, fontWeight: FontWeight.w600),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: width,
+                        child: DropdownButton(
+                          focusColor: const Color(0xffffffff),
+                          dropdownColor: const Color(0xffffffff),
+                          isExpanded: true,
+                          value: dropdownValue,
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          items: categories?.map<DropdownMenuItem<Category>>(
+                              (Category item) {
+                            return DropdownMenuItem<Category>(
+                              value: item,
+                              child: Text(item.name),
+                            );
+                          }).toList(),
+                          onChanged: (Category? value) {
+                            setState(() {
+                              dropdownValue = value!;
+                            });
+                          },
+                        ),
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                            title: const Text('New Category'),
-                            content: TextFormField(
-                              controller: _newCategoryController,
-                            ),
-                            actions: [
-                              ElevatedButton(
-                                onPressed: () {
-                                  isar.when(data: (isar) {
+                      IconButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) => AlertDialog(
+                              title: const Text('New Category'),
+                              content: TextFormField(
+                                controller: _newCategoryController,
+                              ),
+                              actions: [
+                                ElevatedButton(
+                                  onPressed: () {
                                     if (_newCategoryController
                                         .text.isNotEmpty) {
                                       _addCategory(isar);
-                                    } else {}
-                                    return;
-                                  }, error:
-                                      (Object error, StackTrace? stackTrace) {
-                                    return Text(
-                                        'error: $error\nstackTrace: $stackTrace');
-                                  }, loading: () {
-                                    return const CircularProgressIndicator();
-                                  });
-                                },
-                                child: const Text('Add'),
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                      icon: const Icon(Icons.add),
-                    )
-                  ],
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 16.0),
-                  child: Text(
-                    'Title',
-                    style: TextStyle(
-                        color: Colors.black38, fontWeight: FontWeight.w600),
+                                    }
+                                  },
+                                  child: const Text('Add'),
+                                )
+                              ],
+                            ),
+                          );
+                        },
+                        icon: const Icon(Icons.add),
+                      )
+                    ],
                   ),
-                ),
-                TextFormField(
-                  controller: _titleController,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 16.0),
-                  child: Text(
-                    'Start Time',
-                    style: TextStyle(
-                        color: Colors.black38, fontWeight: FontWeight.w600),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Title',
+                      style: TextStyle(
+                          color: Colors.black38, fontWeight: FontWeight.w600),
+                    ),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: width,
-                      child: TextFormField(
-                        controller: _timeController,
-                        enabled: false,
+                  TextFormField(
+                    controller: _titleController,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16.0),
+                    child: Text(
+                      'Start Time',
+                      style: TextStyle(
+                          color: Colors.black38, fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: width,
+                        child: TextFormField(
+                          controller: _timeController,
+                          enabled: false,
+                        ),
                       ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        _selectedTime(context);
+                      IconButton(
+                        onPressed: () {
+                          _selectedTime(context);
+                        },
+                        icon: const Icon(Icons.calendar_month),
+                      ),
+                    ],
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16),
+                  ),
+                  SizedBox(
+                    width: width,
+                    child: DropdownButton(
+                      focusColor: const Color(0xffffffff),
+                      dropdownColor: const Color(0xffffffff),
+                      isExpanded: true,
+                      value: dropdownValueDay,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      items: days.map<DropdownMenuItem<String>>((String item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+                      onChanged: (String? value) {
+                        setState(() {
+                          dropdownValueDay = value!;
+                        });
                       },
-                      icon: const Icon(Icons.calendar_month),
                     ),
-                  ],
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 16),
-                ),
-                SizedBox(
-                  width: width,
-                  child: DropdownButton(
-                    focusColor: const Color(0xffffffff),
-                    dropdownColor: const Color(0xffffffff),
-                    isExpanded: true,
-                    value: dropdownValueDay,
-                    icon: const Icon(Icons.keyboard_arrow_down),
-                    items: days.map<DropdownMenuItem<String>>((String item) {
-                      return DropdownMenuItem<String>(
-                        value: item,
-                        child: Text(item),
-                      );
-                    }).toList(),
-                    onChanged: (String? value) {
-                      setState(() {
-                        dropdownValueDay = value!;
-                      });
-                    },
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 16),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Add'),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16),
                   ),
-                )
-              ],
+                  Align(
+                    alignment: Alignment.center,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Add'),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ));
+          );
+        },
+        error: (Object error, StackTrace? stackTrace) {
+          return Text('error: $error\nstackTrace: $stackTrace');
+        },
+        loading: () {
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
   }
 
   _selectedTime(BuildContext context) async {
@@ -220,11 +226,22 @@ class _CreateRoutinPageState extends ConsumerState<CreateRoutinePage> {
 
   _addCategory(Isar isar) async {
     final categories = isar.categorys;
-
     final newCategory = Category()..name = _newCategoryController.text;
 
-    await isar.writeTxn((isar) async {
+    await isar.writeTxn(() async {
       await categories.put(newCategory);
+    });
+
+    _newCategoryController.clear();
+    _readCategory(isar);
+  }
+
+  _readCategory([Isar? isar]) async {
+    final categoryCollection = isar?.categorys;
+    final getCategories = await categoryCollection?.where().findAll();
+    setState(() {
+      dropdownValue = null;
+      categories = getCategories;
     });
   }
 }
